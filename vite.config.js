ebd8c02253bfa1e915d9466cfcba2b0441ab4961
Artem Ptashenko <aptashenko2019@gmail.com>
2024-05-28 19:04:07 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import {PrimeVueResolver} from 'unplugin-vue-components/resolvers';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/cms-prozorro/',
   plugins: [vue(),
     Components({
       resolvers: [
